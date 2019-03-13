9d2deb12f772058ee2b7997fb3c944fb338832f4
josepereira1 <joseandremp1@gmail.com>
2019-03-13 16:35:10 +0000

    registo de utentes, consultas e serviços

--- jose-1.pl
+++ jose-1.pl
@@ -24,24 +24,20 @@ utente(3,alfredo,45,braga).
 utente(4,dario,72,lisboa).
 utente(5,quim,25,braga).
 
-
-
 % Extenção do predicado servico: IdServ, Descrição, Instituição, Cidade -> { V, F }
 
-servico(1,urgencias,sos1,Braga).
-servico(2,autopsias,sos2,Braga).
-servico(3,geral,sos2,Braga).
-servico(4,especialidade,sos3,Braga).
+servico(1,urgencias,sos1,braga).
+servico(2,autopsias,sos2,braga).
+servico(3,geral,sos2,braga).
+servico(4,especialidade,sos3,braga).
 
+% Extenção do predicado consulta: ID, Data, IdUt, IdServ, Custo -> { V, F }
 
-% Extenção do predicado consulta: Data, IdUt, IdServ, Custo -> { V, F }
-
-consulta(data(10,10,98),1,4,60).
-consulta(data(24,10,98),1,1,23).
-consulta(data(10,10,98),2,2,40).
-consulta(data(10,10,98),3,2,40).
-consulta(data(10,10,98),4,3,30).
-
+consulta(1, data(10,10,98),1,4,60).
+consulta(2, data(24,10,98),1,1,23).
+consulta(3, data(10,10,98),2,2,40).
+consulta(4, data(10,10,98),3,2,40).
+consulta(5, data(10,10,98),4,3,30).
 
 %	funções auxiliares:
 
@@ -51,15 +47,19 @@ comprimento([H|T], R) :- comprimento(T,N), R is N + 1.
 % Invariante Estrutural:  nao permitir a insercao de conhecimento repetido
 %
 
-+utente(ID,N,I,C) :: (solucoes( (ID,N,I,C), (utente(ID,N,I,C)), S),
++utente(ID,_,_,_) :: (solucoes( (ID,_,_,_), (utente(ID,_,_,_)), S),
 					 comprimento( S,N ),
 					 N == 1
 					 ).
++consulta(ID,_,_,_,_) :: (solucoes( (ID,_,_,_,_), (consulta(ID,_,_,_,_)), S),
+						comprimento( S,N ),
+						N == 1
+						).
 
-+filho( F,P ) :: (solucoes( (F,P),(filho( F,P )),S ),
-                  comprimento( S,N ), 
-				  N == 1
-                  ).
++servico(ID,_,_,_) :: (solucoes( (ID,_,_,_), (servico(ID,_,_,_)), S),
+						comprimento( S,N ),
+						N == 1
+						).
 
 
 comprimento([],0).
@@ -71,26 +71,33 @@ teste([I|L]) :- I,teste(L).
 insercao(T) :- assert(T).
 insercao(T) :- retract(T), !, fail.
 
+remocao(T) :- retract(T).
+remocao(T) :- assert(T), !, fail.
+
+nao(T) :- T,insucesso.
+nao(T).
+
 solucoes(N,Condicao,S) :- findall(N,Condicao,S).
 
 evolucao( Termo ) :- solucoes(Invariante, +Termo::Invariante, Lista),
-					 insercao(Termo),
-					 teste(Lista).
+					insercao(Termo),
+					teste(Lista).
+
 
 %--------------------------------- - - - - - - - - - -  -  -  -  -   -
 % Extensao do predicado registarUtente: Id,Nome,Idade,Cidade -> {V,F}
 
-registarUtente(ID,N,I,C) = evolucao(utente(ID,N,I,C)).
+registarUtente(ID,N,I,C) :- evolucao(utente(ID,N,I,C)).
 
 %--------------------------------- - - - - - - - - - -  -  -  -  -   -
 % Extensao do predicado registarServiço: Id,Descrição,Instituição,Cidade -> {V,F}
 
-registarServico(Id,D,I,C).
+registarServico(IDS,D,I,C) :- evolucao(servico(IDS,D,I,C)).
 
 %--------------------------------- - - - - - - - - - -  -  -  -  -   -
 % Extensao do predicado registarConsulta: Data,IdU,IdS,Custo -> {V,F}
 
-registarConsulta(D,IU,IS,C).
+registarConsulta(ID,D,IU,IS,C) :- evolucao(consulta(ID,D,IU,IS,C)).
 
 %--------------------------------- - - - - - - - - - -  -  -  -  -   -
 % Extensao do predicado removerCliente: Id -> {V,F}
